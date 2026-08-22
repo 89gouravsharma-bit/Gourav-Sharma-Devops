b3cab3f4e8b76898f057ec450a13ea2287da8646
Gourav Sharma <89gouravsharma@gmail.com>
2026-08-22 21:33:43 +0530

    correct variable

--- Terraform Learning/pre prod/variable.tf
+++ Terraform Learning/pre prod/variable.tf
@@ -1,12 +1,36 @@
-variable "aaa" {}
+variable "aaa" {
+  type = map(object({
+    rg_name     = string
+    rg_location = string
+  }))
+}
 
 variable "bbb" {
+  type = map(object({
+    stg_name                 = string
+    resource_group_name      = string
+    stg_location             = string
+    account_tier             = string
+    accoubt_replication_type = string
+  }))
 
 }
 variable "ccc" {
+  type = map(object({
+    vnet_name           = string
+    vnet_location       = string
+    resource_group_name = string
+    address_space       = list(string)
+  }))
 
 }
 
 variable "ddd" {
+  type = map(object({
+    subnet_name           = string
+    resource_group_name   = string
+    virttual_network_name = string
+    address_prefixes      = list(string)
+  }))
 
 }
