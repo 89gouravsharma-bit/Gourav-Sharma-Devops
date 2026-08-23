0829584500e16dae1bebd8fc5810b16a92cc3642
Gourav Sharma <89gouravsharma@gmail.com>
2026-08-23 17:33:19 +0530

    correct format

--- Terraform Learning/pre prod/variable.tf
+++ Terraform Learning/pre prod/variable.tf
@@ -27,10 +27,10 @@ variable "ccc" {
 
 variable "ddd" {
   type = map(object({
-    subnet_name           = string
-    resource_group_name   = string
+    subnet_name          = string
+    resource_group_name  = string
     virtual_network_name = string
-    address_prefixes      = list(string)
+    address_prefixes     = list(string)
   }))
 
 }
